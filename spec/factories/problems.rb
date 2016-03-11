74c51925cc1852cf3e370766aa75cde87af16687
Diego Steiner <diego.steiner@renuo.ch>
2016-03-11 21:44:55 +0100

    more specs

--- spec/factories/problems.rb
+++ spec/factories/problems.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :problem do
     data { { key: :value } }
     source { Faker::Internet.url }
+
+    factory :solved_problem do
+      solved_at { 1.day.ago }
+    end
   end
 end
--- spec/models/app_spec.rb
+++ spec/models/app_spec.rb
@@ -1,11 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe App, type: :model do
+  let(:app) { FactoryGirl.create(:app) }
+
   describe '#initialize' do
     let(:app) { FactoryGirl.build(:app) }
     subject { described_class.create!(app.attributes) }
 
     it { is_expected.to be_valid }
     it { is_expected.to have_attributes(name: app.name, slug: app.name.parameterize) }
+  end
+
+  describe '#ok?' do
+    subject { app.ok? }
+
+    context 'with no problems' do
+      it { is_expected.to be(true) }
+    end
+
+    context 'with solved problems' do
+      let!(:problems) { create_list(:solved_problem, 3, app: app) }
+      it { is_expected.to be(true) }
+    end
+
+    context 'with unsolved problems' do
+      let!(:problems) { create_list(:problem, 3, app: app) }
+      it { is_expected.to be(false) }
+    end
   end
 end
